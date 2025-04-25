00522c3faf5120e94a4ca02a8045d44f9c391261
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-04-25 17:36:50 +0300

    Initial Commit

---
+++ OceanScript/OceanScriptApp.swift
@@ -0,0 +1,20 @@
+//
+//  OceanScriptApp.swift
+//  OceanScript
+//
+//  Created by Vlad on 25/4/25.
+//
+
+import SwiftUI
+
+@main
+struct OceanScriptApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
