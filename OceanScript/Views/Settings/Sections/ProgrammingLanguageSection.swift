028f3ffab96cf17b9c8f8e1c491dd60496c8dcaf
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-05-13 20:34:16 +0300

    New section

---
+++ OceanScript/Views/Settings/Sections/ProgrammingLanguageSection.swift
@@ -0,0 +1,18 @@
+//
+//  ProgramminLanguageSection.swift
+//  OceanScript
+//
+//  Created by Vlad on 13/5/25.
+//
+
+import SwiftUI
+
+struct ProgrammingLanguageSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgrammingLanguageSection()
+}
